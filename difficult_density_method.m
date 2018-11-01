110323e90919ebc4ba84d85dea4e53a52a777cb9
hz324 <hz324@tw901.eng.cam.ac.uk>
2018-11-01 23:05:35 +0000

    hey

--- difficult_density_method.m
+++ difficult_density_method.m
@@ -6,7 +6,7 @@ alpha = 1;
 beta = 0.6;
 % Evaluating b and s
 b=(1/alpha)*atan(beta*tan(pi*alpha/2));
-s=(1+(beta^2)*(tan(pi*alpha/2))^2)^(1/(2*alpha));
+s=(1+(beta^2)*(tan(pi*alpha/2))^(2))^(1/(2*alpha));
 % Sampling u and v from uniform and exponential PDFs respectively
 u=(-pi/2) + (pi)*rand(n,1);
 v=exprnd(1,n,1);
@@ -15,6 +15,6 @@ x = s.*((sin(alpha.*(u+b)))./((cos(u)).^(1/alpha))).*((cos(u-alpha*(u+b)))./v).^
 figure(1)
 % Subplot 1 with histograms
 %subplot(211)
-histogram(x,100,'Normalization', 'pdf')
+histogram(x,100)
 legend('numerical rv Histogram')
 hold off
